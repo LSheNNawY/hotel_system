98b9e348207493b477a16d48dc21f9f83562e893
Muhammed Shendi <muhammedshendi@Muhammeds-MacBook-Pro.local>
2021-03-27 17:40:27 +0200

    added country to register

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>AdminLTE 3 | Log in</title>
+    <title>Hotel Management System - Login</title>
     <!-- Tell the browser to be responsive to screen width -->
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
@@ -21,7 +21,7 @@
 <body class="hold-transition login-page">
 <div class="login-box">
     <div class="login-logo">
-        <a href="#"><b>Admin</b>LTE</a>
+        <a href="#"><b>H</b> M S</a>
     </div>
     <!-- /.login-logo -->
     <div class="card">
@@ -88,25 +88,14 @@
                 </div>
             </form>
 
-            <div class="social-auth-links text-center mb-3">
-                <p>- OR -</p>
-                <a href="#" class="btn btn-block btn-primary">
-                    <i class="fab fa-facebook mr-2"></i> Sign in using Facebook
-                </a>
-                <a href="#" class="btn btn-block btn-danger">
-                    <i class="fab fa-google-plus mr-2"></i> Sign in using Google+
-                </a>
-            </div>
-            <!-- /.social-auth-links -->
 
-            <p class="mb-1">
-                @if (Route::has('password.request'))
-                    <a class="btn btn-link" href="#{{--{{ route('password.request') }}--}}">
-                        I forgot my password
-                    </a>
-                @endif
-                <a href="forgot-password.html">I forgot my password</a>
-            </p>
+{{--            <p class="mb-1">--}}
+{{--                @if (Route::has('password.request'))--}}
+{{--                    <a class="btn btn-link" href="#{{ route('password.request') }}">--}}
+{{--                        I forgot my password--}}
+{{--                    </a>--}}
+{{--                @endif--}}
+{{--            </p>--}}
             <p class="mb-0">
                 <a href="{{route("register")}}" class="text-center">Register a new membership</a>
             </p>
